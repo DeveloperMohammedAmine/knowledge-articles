d0421fbcba05b170e27ed53c4a5042d8f43cb4fb
MohammedAmineDeveloper <developermohammed1997@gmail.com>
2024-12-04 22:10:15 +0100

    complete login and register style

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,30 +4,25 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Sign Up Form by Colorlib</title>
+    <title>knowlege Articles - Login</title>
 
     <!-- Font Icon -->
     <link rel="stylesheet" href="{{ asset('auth-assets/fonts/material-icon/css/material-design-iconic-font.min.css') }}">
 
     <!-- Main css -->
     <link rel="stylesheet" href="{{ asset('auth-assets/css/style.css') }}">
+
+    <style>
+        .text-red {
+            color: red;
+        }
+    </style>
+
 </head>
 <body>
 
     <div class="main">
         <!-- Sing in  Form -->
-
-        @if ($errors->any())
-    <div class="alert alert-danger">
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
-@endif
-
-
         <section class="sign-in">
             <div class="container">
                 <div class="signin-content">
@@ -43,10 +38,21 @@
                             <div class="form-group">
                                 <label for="your_name"><i class="zmdi zmdi-account material-icons-name"></i></label>
                                 <input type="text" name="email" id="email" placeholder="Your Email"/>
+                                @error('email')
+                                    <span class="invalid-feedback text-red" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
                             </div>
                             <div class="form-group">
                                 <label for="your_pass"><i class="zmdi zmdi-lock"></i></label>
                                 <input type="password" name="password" id="password" placeholder="Password"/>
+                                @error('password')
+                                    <span class="invalid-feedback text-red" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+
                             </div>
                             <div class="form-group">
                                 <input type="checkbox" name="remember" id="remember" class="agree-term" />
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,104 +1,23 @@
-{{--
-    @extends('layouts.app')
-    
-@section('content')
-<div class="container pt-5">
-    <div style="padding-top: 150px" class="row justify-content-center big-padding-top">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header align-items-center d-flex justify-content-between">
-                    <span>Register</span>
-                    <span>
-                        <a class="btn btn-outline-primary" href="{{ url('login') }}">Login Your Account</a>
-                    </span>
-                </div>
-
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
-
-                        <div class="row mb-3">
-                            <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Name') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="name" placeholder=". . ." type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
-
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" placeholder=". . ." type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" placeholder=". . ." type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" placeholder=". . ." type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="getstarted border-0 m-0">
-                                    {{ __('Register') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-@endsection
---}}
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Sign Up Form by Colorlib</title>
+    <title>knowlege Articles - SignUp</title>
 
     <!-- Font Icon -->
     <link rel="stylesheet" href="{{ asset('auth-assets/fonts/material-icon/css/material-design-iconic-font.min.css') }}">
 
     <!-- Main css -->
     <link rel="stylesheet" href="{{ asset('auth-assets/css/style.css') }}">
+
+    <style>
+        .text-red {
+            color: red;
+        }
+    </style>
+
 </head>
 <body>
     <div class="main">
@@ -107,36 +26,57 @@
             <div class="container">
                 <div class="signup-content">
                     <div class="signup-form">
-                        <h2 class="form-title">Sign up</h2>
-                        <form method="POST" class="register-form" id="register-form">
+                        <h2 class="form-title">Register</h2>
+                        <form method="POST" action="{{ route('register') }}" class="register-form" id="register-form">
+                            @csrf
                             <div class="form-group">
                                 <label for="name"><i class="zmdi zmdi-account material-icons-name"></i></label>
                                 <input type="text" name="name" id="name" placeholder="Your Name"/>
+                                @error('name')
+                                    <span class="invalid-feedback text-red" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
                             </div>
                             <div class="form-group">
                                 <label for="email"><i class="zmdi zmdi-email"></i></label>
                                 <input type="email" name="email" id="email" placeholder="Your Email"/>
+                                @error('email')
+                                    <span class="invalid-feedback text-red" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
                             </div>
                             <div class="form-group">
-                                <label for="pass"><i class="zmdi zmdi-lock"></i></label>
-                                <input type="password" name="pass" id="pass" placeholder="Password"/>
+                                <label for="password"><i class="zmdi zmdi-lock"></i></label>
+                                <input type="password" name="password" id="password" placeholder="Password"/>
+                                @error('password')
+                                    <span class="invalid-feedback text-red" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
                             </div>
                             <div class="form-group">
                                 <label for="re-pass"><i class="zmdi zmdi-lock-outline"></i></label>
-                                <input type="password" name="re_pass" id="re_pass" placeholder="Repeat your password"/>
+                                <input type="password" name="password_confirmation" id="re_pass" placeholder="Repeat your password"/>
                             </div>
                             <div class="form-group">
                                 <input type="checkbox" name="agree-term" id="agree-term" class="agree-term" />
                                 <label for="agree-term" class="label-agree-term"><span><span></span></span>I agree all statements in  <a href="#" class="term-service">Terms of service</a></label>
+                                @error('agree-term')
+                                    <span class="invalid-feedback text-red" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
                             </div>
                             <div class="form-group form-button">
-                                <input type="submit" name="signup" id="signup" class="form-submit" value="Register"/>
+                                <input type="submit" id="signup" class="form-submit" value="Register"/>
                             </div>
                         </form>
                     </div>
                     <div class="signup-image">
                         <figure><img src="{{ asset('auth-assets/images/signup-image.jpg') }}" alt="sing up image"></figure>
-                        <a href="{{ route('register') }}" class="signup-image-link">I am already member</a>
+                        <a href="{{ route('login') }}" class="signup-image-link">I am already member</a>
                     </div>
                 </div>
             </div>
